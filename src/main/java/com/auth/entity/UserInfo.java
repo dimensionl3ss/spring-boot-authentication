d3e054869e44732c32fb0646080b3b8372e8ff01
Adarsh Raj <adarshraj0210@gmail.com>
2023-09-30 01:47:12 +0530

    added getter and setter

--- src/main/java/com/auth/entity/UserInfo.java
+++ src/main/java/com/auth/entity/UserInfo.java
@@ -10,4 +10,23 @@ public class UserInfo {
 	String username;
 	String password;
 	String role;
+	public String getUsername() {
+		return username;
+	}
+	public void setUsername(String username) {
+		this.username = username;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	public String getRole() {
+		return role;
+	}
+	public void setRole(String role) {
+		this.role = role;
+	}
+	
 }
