ba8edd288b3d28e09b2b561f8b9242877a1c1237
Adarsh Raj <adarshraj0210@gmail.com>
2023-09-30 23:54:24 +0530

    Updated security config

--- src/main/java/com/auth/service/UserInfoService.java
+++ src/main/java/com/auth/service/UserInfoService.java
@@ -4,6 +4,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import com.auth.entity.UserInfo;
@@ -13,6 +15,7 @@ import com.auth.repository.UserInfoRepository;
 @Service
 public class UserInfoService implements UserDetailsService {
 	@Autowired private UserInfoRepository userInfoRepository;
+	@Autowired private PasswordEncoder passwordEncoder;
 	@Override
 	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
 		// TODO Auto-generated method stub
@@ -21,6 +24,7 @@ public class UserInfoService implements UserDetailsService {
 	}
 	
 	public String addAUser(UserInfo userInfo) {
+		userInfo.setPassword(passwordEncoder.encode(userInfo.getPassword()));
 		userInfoRepository.save(userInfo);
 		return "User added successfully!";
 	}
