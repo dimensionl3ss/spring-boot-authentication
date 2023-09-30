812b0a8b7b96efda3755eccf7bbbd8b1a42e2da9
Adarsh Raj <adarshraj0210@gmail.com>
2023-10-01 00:34:01 +0530

    Updated Auth Controller and Added Auth Service

--- src/main/java/com/auth/controller/AuthController.java
+++ src/main/java/com/auth/controller/AuthController.java
@@ -4,51 +4,31 @@ package com.auth.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.auth.entity.UserInfo;
-import com.auth.service.UserInfoService;
+import com.auth.service.AuthService;
 import com.auth.utilities.AuthRequest;
 import com.auth.utilities.AuthResponse;
-import com.auth.utilities.JwtService;
 
 
 @RestController
 @RequestMapping("/api/auth")
 public class AuthController {
-	@Autowired private UserInfoService userInfoService; 
-	@Autowired private AuthenticationManager authenticationManager;
-	@Autowired private JwtService jwtService;
+	@Autowired private AuthService authService;
 	@PostMapping("/signup")
 	ResponseEntity<AuthResponse> signup(@RequestBody UserInfo userInfo) {
-		userInfoService.addAUser(userInfo);
-		final String token = jwtService.generateToken(userInfo.getUsername());
-        AuthResponse authResponse = new AuthResponse();
-        authResponse.setStatus(true);
-        authResponse.setToken(token);
-        return new ResponseEntity<AuthResponse>(authResponse, HttpStatus.OK);
-		
+        return new ResponseEntity<AuthResponse>(authService.signupUser(userInfo), HttpStatus.OK);
 	}
 	
 	@PostMapping("/login")
 	ResponseEntity<AuthResponse> login(@RequestBody AuthRequest authRequest) {
-		Authentication authentication = authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(authRequest.getUsername(), authRequest.getPassword()));
-        if (authentication.isAuthenticated()) {
-        	final String token = jwtService.generateToken(authRequest.getUsername());
-            AuthResponse authResponse = new AuthResponse();
-            authResponse.setStatus(true);
-            authResponse.setToken(token);
-            return new ResponseEntity<AuthResponse>(authResponse, HttpStatus.OK);
-        } else {
-            throw new UsernameNotFoundException("invalid user request !");
-        }
+		
+            return new ResponseEntity<AuthResponse>(authService.loginUser(authRequest), HttpStatus.OK);
+        
 	}
 	
 }
---
+++ src/main/java/com/auth/service/AuthService.java
@@ -0,0 +1,44 @@
+package com.auth.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import com.auth.entity.UserInfo;
+import com.auth.repository.UserInfoRepository;
+import com.auth.utilities.AuthRequest;
+import com.auth.utilities.AuthResponse;
+import com.auth.utilities.JwtService;
+
+@Service
+public class AuthService {
+	@Autowired private JwtService jwtService;
+	@Autowired private UserInfoRepository userInfoRepository;
+	@Autowired private AuthenticationManager authenticationManager;
+	
+	public AuthResponse signupUser(UserInfo userInfo) {
+		userInfoRepository.save(userInfo);
+		final String token = jwtService.generateToken(userInfo.getUsername());
+        AuthResponse authResponse = new AuthResponse();
+        authResponse.setStatus(true);
+        authResponse.setToken(token);
+        return authResponse;
+	}
+	
+	public AuthResponse loginUser(AuthRequest authRequest) {
+		Authentication authentication = authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(authRequest.getUsername(), authRequest.getPassword()));
+        if (authentication.isAuthenticated()) {
+        	final String token = jwtService.generateToken(authRequest.getUsername());
+            AuthResponse authResponse = new AuthResponse();
+            authResponse.setStatus(true);
+            authResponse.setToken(token);
+            return authResponse;
+        }
+        else {
+        	throw new UsernameNotFoundException("invalid user request !");
+        }
+	}
+}
