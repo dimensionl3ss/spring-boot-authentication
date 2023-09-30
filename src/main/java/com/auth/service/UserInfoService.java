cf87b9ea5caeae2425409a414f2453848c78eda9
Adarsh Raj <adarshraj0210@gmail.com>
2023-09-30 16:49:25 +0530

    Updated UserInfo service class

--- src/main/java/com/auth/service/UserInfoService.java
+++ src/main/java/com/auth/service/UserInfoService.java
@@ -1,18 +1,27 @@
 package com.auth.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
-
+import com.auth.entity.UserInfo;
+import com.auth.entity.UserInfoDetails;
+import com.auth.repository.UserInfoRepository;
 
 @Service
 public class UserInfoService implements UserDetailsService {
+	@Autowired private UserInfoRepository userInfoRepository;
 	@Override
 	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
 		// TODO Auto-generated method stub
-		return null;
+		UserInfo userInfo = userInfoRepository.findById(username).orElseThrow(() -> new  UsernameNotFoundException("User not found with username: " + username));
+		return new UserInfoDetails(userInfo);
 	}
 	
+	public String addAUser(UserInfo userInfo) {
+		userInfoRepository.save(userInfo);
+		return "User added successfully!";
+	}
 }
